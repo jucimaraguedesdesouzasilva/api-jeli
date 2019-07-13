17c50a8466207af0037b6ef0d69e900ed86e5d15
jucimaraguedesdesouzasilva <guedesjucimara8@gmail.com>
2019-07-13 19:22:57 -0300

    Segunda parte: Pegando dados da api

--- HTML/clima.js
+++ HTML/clima.js
@@ -2,18 +2,34 @@
 
 
 function requestWeather(city_name) {
+
+    city_name = $("#city_input").val();
     $.getJSON({
         url: "https://api.hgbrasil.com/weather/",
         data: {
-            key: 'c9991c56',
+            key: '9988e705',
             format: 'json-cors',
             city_name: city_name,
-            locale: 'pt-br'
+            locale: 'pt'
+
         },
         success: function( result ) {
+            if(result.by === "default"){
+                Toastify({
+                    text: "Cidade inválida, digite uma cidade válida!!!",
+                    duration: 3000,
+                    newWindow: true,
+                    backgroundColor: "#ff0000",
+                  }).showToast();
+            }
+
+            else{
+
+            
             data = result.results;
             city_name = data.city_name;
-
+  
+            
             $( "#city" ).html(  city_name);
             $( "#temp" ).html( data.temp +"°C" )
             $( "#wind" ).html("Velocidade do vento:"+  data.wind_speedy);
@@ -29,11 +45,27 @@ function requestWeather(city_name) {
             var url_img = "https://assets.hgbrasil.com/weather/images/" + data.img_id + ".png";
             $( "#weather-img" ).attr("src",url_img);
         }
+        }
 
     })
 }
 
 
+
+
 $( document ).ready(function() {
-    requestWeather();
+    $('#city_input').on('keypress',function(e) {
+        if(e.which == 13) {
+            var city_name = $("#city_input").val()
+            requestWeather(city_name);
+        }
+
+        
+        
+    });
+
+    
+
+
+
 });
